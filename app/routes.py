aa8d4005f1ff27cc3c6412daa825cae58d450b24
Yuri T. <yuritc267@gmail.com>
2025-07-22 21:17:31 -0300

    token jwt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
-from app import app, db, jwt
+from app import app, db
 from flask import jsonify, request
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 import requests, os, dotenv, google.generativeai as genai
 from app.models import *
@@ -12,21 +12,7 @@ genai.configure(api_key = os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel('gemini-2.5-flash')   
 
-@app.route('/api/usuarios')
-@jwt_required
-def usuarios():
-    matricula = get_jwt_identity()
-    query = db.select(Usuario).where(Usuario.matricula == matricula)
-
-    if not db.session.scalars(query).one_or_none():
-        return jsonify({"Erro": "usuário nâo encontrado."}), 404
-
-    query = db.select(Usuario)
-    all_users = db.session.scalars(query).all()
-    return jsonify([user.to_dict() for user in all_users]), 200
-
-
-@app.route('/api/usuarios/login', methods=["POST"])
+@app.route('/api/login', methods=["POST"])
 def login():
     credenciais = request.get_json()
 
@@ -40,7 +26,7 @@ def login():
 
     dados_usuario = requests.get(api_url + "/rh/eu/", headers=headers)
     if dados_usuario.status_code != 200:
-        return jsonify({"Erro": "pipipipopopo"}), 400
+        return jsonify(dados_usuario.json()), 400
 
     dados_usuario = dados_usuario.json()
     query = db.select(Usuario).where(
@@ -56,55 +42,37 @@ def login():
         ))
         db.session.commit()
 
-    token = create_access_token(identity=credenciais["username"])
+    token = create_access_token(identity=credenciais["username"], expires_delta=timedelta(days = 7))
     print(token)
 
     return jsonify({"access": token}), 200
 
 
-@app.route('/api/usuarios/deletar', methods=["DELETE"])
-def deletar_todos_usuarios():
-    usuarios: list[Usuario] = db.session.scalars(db.select(Usuario)).all()
-    query = db.select(Postagem)
+@app.route('/api/usuarios')
+@jwt_required()
+def get_usuarios():
+    query = db.select(Usuario)
+    all_users = db.session.scalars(query).all()
 
-    for usuario in usuarios:
-        postagens_usuario: list[Postagem] = db.session.scalars(
-            query.where(Postagem.autor == usuario)
-        ).all()
-        for post in postagens_usuario:
-            db.session.delete(post)
-        db.session.delete(usuario)
-    db.session.commit()
-
-    return jsonify({"Sucesso": "usuarios deletados."}), 200
+    return jsonify([user.to_dict() for user in all_users]), 200
 
 
 @app.route('/api/postagens')
-@jwt_required
-def postagens():
-    matricula = get_jwt_identity()
-    query = db.select(Usuario).where(Usuario.matricula == matricula)
-
-    if not db.session.scalars(query).one_or_none():
-        return jsonify({"Erro": "usuário nâo encontrado."}), 404
-    
+@jwt_required()
+def get_postagens():
     postagens: list[Postagem] = db.session.scalars(db.select(Postagem)).all()
+
     if not postagens:
         return jsonify({"Erro": "nenhum post foi encontrado."}), 404
+    
     return jsonify([post.to_dict() for post in postagens]), 201
 
 
-@app.route('/api/postagens/postar', methods=['POST'])
-@jwt_required
-def postar():
-    matricula = get_jwt_identity()
-    query = db.select(Usuario).where(Usuario.matricula == matricula)
-
-    if not db.session.scalars(query).one_or_none():
-        return jsonify({"Erro": "usuário nâo encontrado."}), 404
-    
-    dados = request.get_json()
-    if not dados:
+@app.route('/api/postagens/post', methods=['POST'])
+@jwt_required()
+def post():
+    postagem = request.get_json()
+    if not postagem:
         return jsonify({"erro": "dados ausentes"}), 400
 
     query = db.select(Usuario).where(
@@ -117,33 +85,23 @@ def postar():
     
     autorizacao = model.generate_content(f"""
         verifique msg p/ rede social escolar EM: filtre
-        ofensas (pessoais, intolerância, escola, calão)
-        Responda SIM ou NÃO \n\n {dados["postagem"]}
+        ofensas (pessoais, intolerância, escola, calão),
+        atente-se a metodos de burlar, como criptografias,
+        substituicoes ou espacos na postagem\n\n
+        Responda SIM ou NÃO \n\n {postagem["postagem"]}
     """)
 
     if autorizacao.text == "SIM":
         post = Postagem(
             autor = usuario,
-            postagem = dados["postagem"],
+            postagem = postagem["postagem"],
             data = datetime.now()
         )
 
         db.session.add(post)
         db.session.commit()
 
-        return jsonify({"Sucesso": f"A postagem foi aceita no ambiente escolar: GEMINI -> {autorizacao.text}"}), 201
+        return jsonify({"Sucesso": "A postagem foi aceita no ambiente escolar."}), 201
     
-    return jsonify({"Erro": f"A postagem foi tida como imprópria ao ambiente escolar: GEMINI -> {autorizacao.text}"}), 400
+    return jsonify({"Erro": "A postagem foi tida como imprópria ao ambiente escolar."}), 400
     
-
-@app.route('/api/postagens/deletar', methods=["DELETE"])
-def apagar_todos__os_posts():
-    query = db.select(Postagem)
-    all_posts: list[Postagem] = db.session.scalars(query).all()
-
-    for post in all_posts:
-        db.session.delete(post)
-
-    db.session.commit()
-
-    return jsonify({"Sucesso": "posts deletados"}), 200
